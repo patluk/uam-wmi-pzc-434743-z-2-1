c4990440bb50577023f8eba3914cf85d3a697187
unknown <patluk6@st.amu.edu.pl>
2023-12-01 23:22:17 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import datetime
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_items():
+    html = """
+    <html>
+    <head>
+        <title>Rozwiązanie zadania PZC 2.1</title>
+    </head>
+    <body>
+    """
+    html += str(datetime.datetime.now(tz=datetime.timezone.utc).isoformat())
+    html +=  """
+    </body>
+    """
+
+    return html
